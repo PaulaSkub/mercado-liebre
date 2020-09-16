54eb807977dbfd6d8df593e0cdb8dc758d6d1d5b
Paulaskub <poli.skubic@gmail.com>
2020-09-15 21:38:05 -0300

    clase uno solucion

---
+++ app.js
@@ -0,0 +1,27 @@
+
+const express = require('express')
+const app = express()
+const path = require("path")
+
+app.get ('/', function (req,res){
+    let file = path.resolve('app.html')
+    res.sendFile(file);
+ })
+
+app.get ('*', function (req,res){
+    if (req.url.endsWith ('.css')){
+        let file = path.resolve('public/styles' + req.url);
+        return res.sendFile(file);
+    } 
+
+    let imagenes = ['jpg','jpeg','gif','png','svg'];
+    let ext = req.url.split('.')[1];
+             if(imagenes.includes(ext)){
+               let file = path.resolve ('public/imagenes'+ req.url) 
+               return res.sendFile(file) 
+             }
+
+})
+ 
+ app.listen(3000)
+
