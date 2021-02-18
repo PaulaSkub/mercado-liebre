639eca8bf64a4ba1123132a0466f00ab93cf8a5b
Paulaskub <poli.skubic@gmail.com>
2021-02-18 11:32:47 -0300

    Agregando logica para api latest

--- src/controllers/api/productsController.js
+++ src/controllers/api/productsController.js
@@ -5,7 +5,24 @@ const { Product, Brand, Category } = require('../../database/models');
 
 module.exports = {
 	latest (req, res,next){
-
+       
+        Product.findAll({
+			order: [
+				['createdAt', 'DESC']
+			],
+			limit: 8
+		})
+		
+			.then(function(products){
+				let respuesta = {
+					meta:{
+						status: 200,
+						count: products.length,
+						url: "/api/products/latest"
+						},
+					data: products}
+			 res.send(respuesta)})
+			.catch(e => console.log(e));
 
     },
     offers (req, res){
--- src/routes/api/productsRouter.js
+++ src/routes/api/productsRouter.js
@@ -4,10 +4,7 @@ const router = express.Router();
 
 const productsApiController = require('../../controllers/api/productsController');
 
-
 router.get('/latest', productsApiController.latest);
 router.get('/offers', productsApiController.offers);
-
-
 
 module.exports = router;
