59e1d6ea87e6f119e807fc6739380f68616cb002
Paulaskub <poli.skubic@gmail.com>
2021-02-18 09:51:54 -0300

    Agregando el archivo de apis a la carpeta controller

---
+++ src/controllers/api/productsController.js
@@ -0,0 +1,16 @@
+const { validationResult } = require('express-validator');
+const createError = require('http-errors');
+const { Product, Brand, Category } = require('../../database/models');
+
+
+module.exports = {
+	latest (req, res,next){
+
+
+    },
+    offers (req, res){
+
+		
+    }
+    
+};
