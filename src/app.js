00ef808426d0524b2ce92570d4f836af68f750fa
Paulaskub <poli.skubic@gmail.com>
2021-02-18 09:48:34 -0300

    Agregando las rutas en el archio app.js

--- src/app.js
+++ src/app.js
@@ -43,10 +43,12 @@ app.use(helpersMiddleware);
 const mainRouter = require('./routes/mainRouter'); // Rutas main
 const productsRouter = require('./routes/productsRouter'); // Rutas /products
 const usersRouter = require('./routes/usersRouter'); // Rutas /user
+const productsapiRouter = require('./routes/api/productsRouter') // Rutas /apis para productos
 
 app.use('/', mainRouter);
 app.use('/products', productsRouter);
 app.use('/users', usersRouter);
+app.use('/api/products', productsapiRouter)
 
 // ************ DON'T TOUCH FROM HERE ************
 // ************ catch 404 and forward to error handler ************
