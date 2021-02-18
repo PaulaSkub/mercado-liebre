444045f44b81aea18987244b8902eb8ed0d0436c
Paulaskub <poli.skubic@gmail.com>
2021-02-18 09:50:14 -0300

    Agregando las rutas de la api productos en la carpeta rutes con su logica

---
+++ src/routes/api/productsRouter.js
@@ -0,0 +1,13 @@
+// ************ Require's ************
+const express = require('express');
+const router = express.Router();
+
+const productsApiController = require('../../controllers/api/productsController');
+
+
+router.get('/latest', productsApiController.latest);
+router.get('/offers', productsApiController.offers);
+
+
+
+module.exports = router;
