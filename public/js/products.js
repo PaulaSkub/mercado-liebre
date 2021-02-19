6453c5de584aeb1d7ddb708b9a441a67562b8005
Paulaskub <poli.skubic@gmail.com>
2021-02-19 17:07:25 -0300

    Modificando archivos para mostrar los productos con oferta con los datos que llegan de /api/products/offers

--- public/js/products.js
+++ public/js/products.js
@@ -40,3 +40,42 @@ fetch('http://localhost:3000/api/products/latest')
           );
 
 
+          fetch('http://localhost:3000/api/products/offers')
+		.then(response => response.json())
+		.then (products => {
+            
+            let latestProduct = document.getElementById('offers-products')
+            let productdata = products.data
+            let variableNueva = ""
+
+            productdata.forEach( item =>{
+                let price = item.price - item.price * item.discount / 100
+                
+
+                variableNueva += `<div class="col-12 col-sm-6 col-lg-3">
+                <section class="product-box">
+                    <a href="/products/detail/ ${item.id} ">
+                        <figure class="product-box_image">
+                            <img src="/images/products/${item.image}" alt="${item.name}">
+                        </figure>
+                        <article class="product-box_data">
+                            <h2>${price}</h2>
+                            <span>${item.discount} % OFF</span>
+                            <p>${item.name}</p>
+                            <i class="fas fa-truck"></i>
+                        </article>
+                    </a>
+                </section>
+            </div>`
+                           } )
+
+                           latestProduct.innerHTML =   variableNueva      
+
+                 console.log(variableNueva)
+        
+        })
+        .catch(e =>
+            console.log(e)
+          );
+
+
